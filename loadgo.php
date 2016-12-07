9034cc0f8745832c039f55d4f1107749d1bb64c5
Fran Verona <franverona@gmail.com>
2016-12-07 15:15:59 +0000

    Fix version number on main plugin file

--- loadgo.php
+++ loadgo.php
@@ -6,7 +6,7 @@ Text Domain: loadgo-for-wp
 Domain Path: /languages
 Plugin URI: http://franverona.com/loadgo
 Description: Create an automatic page load progress bar using <a href="http://github.hubspot.com/pace/docs/welcome/">PACE</a> and <a href="http://franverona.com/loadgo">Loadgo</a> Javascript plugin.
-Version: 1.1
+Version: 1.3
 Author: Fran Verona
 Author URI: http://franverona.com
 */
